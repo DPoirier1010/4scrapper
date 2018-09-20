154e810042c281be744dd3ead47709bab8ceb5de
DPoirier1010 <43175085+DPoirier1010@users.noreply.github.com>
2018-09-20 13:07:04 -0400

    Working

---
+++ scrapper.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import urllib
+from urllib.request import Request, urlopen
+
+#Author raider1010
+#https://youtu.be/m_agcM_ds1c
+#4chan Scrapper 1.0
+
+
+
+hdr = {'User-Agent':'Mozilla/5.0'}
+site = input("copy the url         :") #### path to a thread the thread you want all image
+counter = 1
+req = Request(site,headers=hdr)
+page = urlopen(req)
+soup = BeautifulSoup(page, "html.parser")
+
+#for script in soup(["script"]):
+#    script.decompose() 
+
+allImg = soup.findAll("a", class_="fileThumb") #### find all the url for the images
+for i in allImg :
+    image=i.get('href')
+    image2 = image
+    image = 'http:' + image
+    req2 = Request(image,headers=hdr)
+    page2 = urlopen(req)
+    soup2 = BeautifulSoup(page2, "html.parser")
+    filename = str(counter)
+    counter = counter+1
+    print(counter)
+
+
+    imageFile = open(filename + ".png", 'wb')
+    imageFile.write(urllib.request.urlopen(image).read())
+    imageFile.close()
